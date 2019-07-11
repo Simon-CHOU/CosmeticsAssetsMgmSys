2dd2dceea5186e9439e73bb77935e473f78b31d8
Simon-Chou <mrsimonzhou@gmail.com>
2019-07-12 03:51:33 +0800

    数据库结构和数据

---
+++ public.sql
@@ -0,0 +1,17 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本机PostgreSQL
+ Source Server Type    : PostgreSQL
+ Source Server Version : 110004
+ Source Host           : localhost:5432
+ Source Catalog        : test
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 110004
+ File Encoding         : 65001
+
+ Date: 12/07/2019 03:34:27
+*/
+
