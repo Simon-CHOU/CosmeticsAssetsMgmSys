d61be229ff3f1cf1643a88b3372b31214c4b113f
Simon-Chou <mrsimonzhou@gmail.com>
2019-07-11 16:52:11 +0800

    补全Dao

---
+++ src/main/java/com/simon/assets/dao/AssetMapper.java
@@ -0,0 +1,7 @@
+package com.simon.assets.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.simon.assets.entity.Asset;
+
+public interface AssetMapper extends BaseMapper<Asset> {
+}
---
+++ src/main/java/com/simon/assets/dao/InRec.java
@@ -0,0 +1,6 @@
+package com.simon.assets.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface InRec extends BaseMapper<InRec> {
+}
