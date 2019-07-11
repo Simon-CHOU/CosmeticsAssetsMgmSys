7b6ada8fc8a8c313b9bd8909b9494938c9e9396a
Simon-Chou <mrsimonzhou@gmail.com>
2019-07-11 17:49:33 +0800

    测试分页查询

--- src/test/java/com/simon/assets/SampleTest.java
+++ src/test/java/com/simon/assets/SampleTest.java
@@ -1,8 +1,13 @@
 package com.simon.assets;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.simon.assets.dao.UserMapper;
 import com.simon.assets.entity.SysUser;
+import org.apache.ibatis.annotations.Param;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,6 +32,19 @@ public class SampleTest {
         System.out.println(sysUserList);
         Assert.assertEquals(5, sysUserList.size());
         sysUserList.forEach(System.out::println);
+    }
+
+    @Test
+    public void selectPage() {
+        IPage<SysUser> page = new Page<SysUser>(1,3);
+        QueryWrapper<SysUser> queryWrapper = new QueryWrapper<SysUser>();
+        queryWrapper.eq("age",20);// 设置where 条件 https://mybatis.plus/guide/wrapper.html
+
+        IPage<SysUser> iPage  = userMapper.selectPage(page, queryWrapper);
+        System.out.println("总页数："+ iPage.getPages());
+        System.out.println("总记录数："+ iPage.getTotal());
+        List<SysUser> userList = iPage.getRecords();
+        userList.forEach(System.out::println);
     }
 
 }
