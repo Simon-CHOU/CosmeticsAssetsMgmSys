babeb12b3b2fdcc83ac999db0dc2283201880376
Simon-Chou <mrsimonzhou@gmail.com>
2019-07-12 15:11:26 +0800

    署名

--- src/main/java/com/simon/assets/CosmeticsAssetsMgmSysApplication.java
+++ src/main/java/com/simon/assets/CosmeticsAssetsMgmSysApplication.java
@@ -4,6 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * date:20190712
+ * author:simon
+ */
 @SpringBootApplication
 @MapperScan("com.simon.assets.dao")
 public class CosmeticsAssetsMgmSysApplication {
